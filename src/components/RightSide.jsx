9bd0990c86874205cac9aa1c3fe4172ebc0c9df7
Gajdeg <38560607+ger11kadiu@users.noreply.github.com>
2022-12-05 14:10:39 +0100

    .

--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -4,6 +4,8 @@ export default function RightSide() {
   return (
     <div className="rightSide">
       <div className="center"></div>
+      <div className="bigCircle"></div>
+      <div className="copyRight">Copyright ©2022. All rights reserved.</div>
     </div>
   );
 }
